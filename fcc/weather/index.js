44a11559b9a4c32c3a01539a4e3729b2f124f01b
weblzf <1505994869@qq.com>
2017-04-29 09:35:03 +0800

    天气

---
+++ fcc/weather/index.js
@@ -0,0 +1,15 @@
+// $.ajax({
+//     type:'GET',
+//     dataType:'json',
+//     url:'http://aider.meizu.com/app/weather/listWeather?cityIds=101240101',
+//     jsonpCallback:'callback',
+//     success:function data(data){
+//         console.log(data)
+//     },
+//     error:function(e){
+//         console.log(e)
+//     }
+// })
+// function callback(){
+//     console.log(arguments);
+// }
